ab1a1e75f9f4802ca9c0417b279b9bdc10e859fc
Franz Haas <franz.dominik.haas@gmail.com>
2025-02-08 13:22:20 +0100

    cleaned async example

--- atest/async/async_demo.py
+++ atest/async/async_demo.py
@@ -1,21 +1,33 @@
 import asyncio
 from robot.api.deco import keyword
 import robot.libraries.BuiltIn as BuiltIn
+from robot.api import logger
+import logging
+import threading
 
 
 class async_demo():
     def __init__(self):
         super().__init__()
+        BuiltIn.BuiltIn().run_keyword("Log", f"loading  {threading.current_thread().ident}", "INFO")
     
     async def _task_async(self):
         await asyncio.sleep(1)
-        BuiltIn.BuiltIn().run_keyword("Log", "Slept first 1s", "INFO")
+        BuiltIn.BuiltIn().run_keyword("Log", f"Slept first 1s  {threading.current_thread().ident}", "INFO")
         await asyncio.sleep(2)
-        BuiltIn.BuiltIn().run_keyword("Log", "Slept second 2s", "INFO")
+        logger.info(f"Slept second 2s {threading.current_thread().ident}")
+        logging.info("logging from python")
+        print("*INFO* print from python")
 
     @keyword
     async def  start_async_wait(self):
+        await asyncio.sleep(1)
         self._task = asyncio.create_task(self._task_async())
+
+    @keyword
+    async def  start_async_wait_with_return(self):
+        await asyncio.sleep(1)
+        return 1
 
     @keyword
     async def wait_for_events(self):
--- atest/async/async_demo.robot
+++ atest/async/async_demo.robot
@@ -2,8 +2,10 @@
 Library      async_demo.py
 *** Test Cases ***
 base demo
+    ${R}=     start_async_wait_with_return
     start_async_wait
     start_async_wait
     Log    outside of async
+    Sleep      0s
     wait_for_events
     Log    outside of async
